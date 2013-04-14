fc84b6796a16b62fe6baaf83e550260dd5244425
Abhas Bhattacharya <abhasbhattacharya2@gmail.com>
2013-04-15 04:05:32 +0530

    nnf and sif to json converter (python)

---
+++ nnf_and_sif_to_json_py/readme.txt
@@ -0,0 +1,2 @@
+nnf_to_json.py takes galFiltered.nnf as input as produces result.json as output.
+sif_to_json.py takes galFiltered.sif as input as produces result2.json as output.
---
+++ nnf_and_sif_to_json_py/sif_to_json.py
@@ -0,0 +1,28 @@
+r=open("galFiltered.sif","r")
+w=open("result.json","w")
+nodes=[]
+w.write("[\n")
+for p in r:
+    if nodes:
+        if not gave_newline:
+            gave_newline=True
+            w.write(",\n")
+    temp=p[:-1].split(" ")
+    if temp[0] not in nodes:
+        nodes.append(temp[0])
+        w.write('{ data: { id: "'+temp[0]+'" }, group: "nodes" }')
+        gave_newline=False
+    if len(temp)>1:
+        if temp[2] not in nodes:
+            if not gave_newline:
+                w.write(",\n")
+            nodes.append(temp[2])
+            w.write('{ data: { id: "'+temp[2]+'" }, group: "nodes" },\n')
+            gave_newline=True
+    if not gave_newline:
+        w.write(",\n")
+    w.write('{ data: { id: "'+ temp[0]+" ("+temp[1]+") "+temp[2]+'", source: "'+temp[0]+'", target: "'+temp[2]+'" }, group: "edges" }')
+    gave_newline=False
+
+w.write("\n]")
+w.close()
